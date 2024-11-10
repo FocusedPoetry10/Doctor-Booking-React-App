1bcb588f7e2eb770acb3de657c363b6094389258
VishwaMoorthy12 <sivaanandhi397@gmail.com>
2024-11-11 01:19:26 +0530

    Faq section done!

--- frontend/src/Components/Faq/FaqItem.jsx
+++ frontend/src/Components/Faq/FaqItem.jsx
@@ -11,12 +11,12 @@ const FaqItem = ({ item }) => {
     }
 
     return(
-        <div className="p-3 lg:p-5 rounded-[12px] border-solid border-[#D9DCE2] mb-5 cursor-pointer">
+        <div className="p-3 lg:p-5 rounded-[12px] border border-solid border-[#D9DCE2] mb-5 cursor-pointer">
             <div className="flex items-center justify-between gap-5" onClick={toggleAccordion}>
                 <h4 className="text-[16px] leading-7 lg:text-[22px] lg:leading-8 text-headingColor">
                     {item.question}
                 </h4>
-              <div className={' $ {isOpen && "bg-primaryColor text-white border-none" } w-7 h-7 lg:w-8 lg:h-8 border-solid border-[#141F21] rounded flex items-center justify-center'}>
+              <div className={' $ {isOpen && "bg-primaryColor text-white border-none" } w-7 h-7 lg:w-8 lg:h-8 borderborder-solid border-[#141F21] rounded flex items-center justify-center'}>
                 {isOpen ? <AiOutlineMinus /> : <AiOutlinePlus />}
                 </div>  
             </div>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -293,7 +293,22 @@ const Home = () => {
 </section>
 {/* ==== faq section end ==== */}
 
-    
+{/* ====== testimonial ======== */}
+<section>
+    <div className="container">
+    <div className="xl:w-[470px] mx-auto">
+        <h2 className="heading text-center">What our patient say</h2>
+        <p className="text__para text-center">
+            World-class care for everyone. Our health system offers unmatched, expert health care.
+        </p>
+    </div>
+
+    </div>
+</section>
+
+{/* ====== testimonial end ======== */}
+
+
 
 
    </>
