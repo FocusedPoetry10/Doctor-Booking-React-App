1449c225ee1e182377ddf865d39d0f9b4764d92d
VishwaMoorthy12 <sivaanandhi397@gmail.com>
2024-11-13 00:18:15 +0530

    Doctor About pg

--- frontend/src/pages/Doctors/DoctorAbout.jsx
+++ frontend/src/pages/Doctors/DoctorAbout.jsx
@@ -1,7 +1,35 @@
 import React from "react";
 
 const DoctorAbout = () => {
-    return <div>DoctorAbout</div>
+    return (
+        <div>
+            <div>
+                <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold flex items-center gap-2">
+                    About of
+<span className="text-irisBlueColor font-bold text-[24px] leading-9">Srinivasan</span>
+                </h3>
+                <p className="text__para">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum, qui incidunt ullam ab placeat quos assumenda exercitationem sequi commodi nesciunt ipsum laborum repudiandae. Voluptatem ratione neque sed dolor, sit, reprehenderit tempora ipsum nulla consequatur ducimus doloremque iusto aspernatur quo sapiente magni assumenda rem aliquam magnam voluptas alias repudiandae qui fugit?
+             
+                </p>
+            </div>
+            <div className="mt-12">
+                <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold"> Education </h3>
+
+                <ul className="pt-4 md:p-5">
+                    <li className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]">
+                     <div>
+                        <span className="text-irisBlueColor text-[15px] leading-6 font-semibold">12 November, 2024
+                        </span>
+                        <p className="text-[16px] leading-6 font-medium text-textColor">PHD in Surgeon</p>
+                     </div>
+                     <p className="text-[14px] leading-5 font-medium text-textColor">New Apollo Hospital, New York.</p>
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+    );
+   
 };
 
 export default DoctorAbout;
