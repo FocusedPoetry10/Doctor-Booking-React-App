f3a5a6babe1bbf107d790e10ff66266b4a88522a
VishwaMoorthy12 <sivaanandhi397@gmail.com>
2024-11-13 00:47:42 +0530

    Doctor About pg

--- frontend/src/pages/Doctors/DoctorAbout.jsx
+++ frontend/src/pages/Doctors/DoctorAbout.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { formateDate } from "../../utils/formateDate";
 
 const DoctorAbout = () => {
     return (
@@ -18,15 +19,47 @@ const DoctorAbout = () => {
                 <ul className="pt-4 md:p-5">
                     <li className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]">
                      <div>
-                        <span className="text-irisBlueColor text-[15px] leading-6 font-semibold">12 November, 2024
+                        <span className="text-irisBlueColor text-[15px] leading-6 font-semibold"> {formateDate("12-11-2024")} = {formateDate("12-11-2025")}
                         </span>
                         <p className="text-[16px] leading-6 font-medium text-textColor">PHD in Surgeon</p>
+                     </div>
+                     <p className="text-[14px] leading-5 font-medium text-textColor">New Apollo Hospital, New York.</p>
+                    </li>
+                    <li className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]">
+                     <div>
+                        <span className="text-irisBlueColor text-[15px] leading-6 font-semibold"> {formateDate("11-11-2024")} = {formateDate("11-11-2025")}
+                        </span>
+                        <p className="text-[15px] leading-6 font-medium text-textColor">PHD in Surgeon</p>
                      </div>
                      <p className="text-[14px] leading-5 font-medium text-textColor">New Apollo Hospital, New York.</p>
                     </li>
                 </ul>
 
             </div>
+
+        <div className="mt-12">
+            <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold">
+                Experience
+            </h3>
+
+            <ul className="grid sm:grid-cols-2 gap-[30px] pt-4 md:p-5">
+                <li className="p-4 rounded bg-[#ff9ea]">
+                    <span className="text-yellowColor text-[15px] leading-6 font-semibold">
+                    {formateDate("11-11-2024")} = {formateDate("11-11-2025")}
+                    </span>
+                    <p className="text-[16px] leading-6 font-medium text-textColor">Sr. Surgeon</p>
+                    <p className="text-[14px] leading-5 font-medium text-textColor">New Apollo Hospital, New York.</p>
+                </li>
+                <li className="p-4 rounded bg-[#ff9ea]">
+                    <span className="text-yellowColor text-[15px] leading-6 font-semibold">
+                    {formateDate("11-11-2024")} = {formateDate("11-11-2025")}
+                    </span>
+                    <p className="text-[16px] leading-6 font-medium text-textColor">Sr. Surgeon</p>
+                    <p className="text-[14px] leading-5 font-medium text-textColor">New Apollo Hospital, New York.</p>
+                </li>
+            </ul>
+        </div>
+
         </div>
     );
    
