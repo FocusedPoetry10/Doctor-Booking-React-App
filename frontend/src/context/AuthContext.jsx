cfa1bb164da9ebc005aac6f6f6583af65bd7a847
FocusedPoetry10 <vinee3700@gmail.com>
2024-11-16 14:05:25 +0530

    Adding the modified AuthContext.jsx again!!

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -3,11 +3,9 @@ import { createContext, useContext, useReducer, useEffect } from "react";
 // Initial state
 const user = localStorage.getItem('user');
 const initialState = {
-    user: localStorage.getItem('user') === undefined 
-    ? JSON.parse(localStorage.getItem('user'))
-    : null,
+    user: user ? JSON.parse(user) : null,
     role: localStorage.getItem('role') || null,
-    token: localStorage.getItem("token") || null,
+    token: localStorage.getItem('token') || null,
 };
 
 // Create Context
@@ -45,9 +43,9 @@ const authReducer = (state, action) => {
     }
 };
 
-
-export const authContextProvider = ({ children }) => {
-    const { state, dispatch } = useReducer(authReducer, initialState)
+// AuthContext Provider
+export const AuthContextProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(authReducer, initialState);
 
     useEffect(() => {
         localStorage.setItem('user', JSON.stringify(state.user))
