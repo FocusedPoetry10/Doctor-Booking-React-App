06eb5a1662975554c7e1208786a0f3dbebdc8719
FocusedPoetry10 <vinee3700@gmail.com>
2024-11-17 12:53:12 +0530

    Adding 3 modified files with the new Profile page. So, Four files!!

--- frontend/src/pages/Doctors/DoctorAbout.jsx
+++ frontend/src/pages/Doctors/DoctorAbout.jsx
@@ -1,6 +1,6 @@
 import { formateDate } from "../../../utils/formateDate.js";
 
-const DoctorAbout = () => {
+const DoctorAbout = ({ name, about, qualifications, experiences }) => {
     return (
         <div>
             {/* About Section */}
@@ -18,7 +18,7 @@ const DoctorAbout = () => {
             </section>
 
             {/* Education Section */}
-            <section className="mt-12">
+            
                 <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold">Education</h3>
                 <ul className="pt-4 md:p-5">
                     <li className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]">
@@ -40,10 +40,10 @@ const DoctorAbout = () => {
                         <p className="text-[14px] leading-5 font-medium text-textColor">New Apollo Hospital, New York.</p>
                     </li>
                 </ul>
-            </section>
+           
 
             {/* Experience Section */}
-            <section className="mt-12">
+  
                 <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold">Experience</h3>
                 <ul className="grid sm:grid-cols-2 gap-[30px] pt-4 md:p-5">
                     <li className="p-4 rounded bg-[#ff9ea]">
@@ -61,7 +61,7 @@ const DoctorAbout = () => {
                         <p className="text-[14px] leading-5 font-medium text-textColor">New Apollo Hospital, New York.</p>
                     </li>
                 </ul>
-            </section>
+
         </div>
     );
 };
