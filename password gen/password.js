504cf03bbb127d0cbc87b1fcac810cf332679bb6
Vineeth Kumar <135852071+VineethKumar-2003@users.noreply.github.com>
2024-12-10 14:49:46 +0530

    Update and rename password.js to none.js

--- password gen/password.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-const password = 'Vishwa'; // Replace with the desired password
-const saltRounds = 10;
-
-bcrypt.hash(password, saltRounds, (err, hash) => {
-    if (err) throw err;
-    console.log('Hashed Password:', hash);
-});
